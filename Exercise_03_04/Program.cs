92de9d3c79fdcf6d6074c6b2774ef5021c7493d5
Nika <nikarasevas@gmail.com>
2022-10-13 21:34:04 +0300

    Рекурсия: 1) возвращаем сумму цифр числа / 2) возводим A в степень B

---
+++ Exercise_03_04/Program.cs
@@ -0,0 +1,46 @@
+﻿/* Задача 3: Напишите программу, которая будет принимать на вход число и 
+возвращать сумму его цифр.
+453 -> 12
+45 -> 9 */ 
+
+
+// Console.WriteLine("Введите число: ");
+// int m = int.Parse(Console.ReadLine()!);
+
+// int SumRecur(int x)
+// {
+//     if(x == 0) 
+//     {
+//         return 0;
+//     }
+//     return(x % 10 + SumRecur(x / 10));
+// } 
+
+// Console.WriteLine(SumRecur(m));
+
+
+/* Задача 4: Напишите программу, которая на вход принимает два числа A и B, 
+и возводит число А в целую степень B с помощью рекурсии.
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8 */
+
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите степень числа b: ");
+int b = int.Parse(Console.ReadLine()!);
+
+int PowRecur(int x, int y)
+{
+    if(y == 0)
+    {
+        return 1;
+    }
+    if(y == 1)
+    {
+        return x;
+    }
+    return(PowRecur(x, y - 1) * x);
+}
+Console.WriteLine();
+Console.WriteLine(PowRecur(a, b));
